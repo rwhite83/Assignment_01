42073f3bb1e982e580c50225f3eea07939f1ebce
rwhite83 <rwhite83@hotmail.com>
2019-02-08 20:56:21 -0800

    final finished product i will be submitting after pushing to master

--- connectivity_matrix.hpp
+++ connectivity_matrix.hpp
@@ -17,5 +17,5 @@ public:
     Connectivity_matrix(int single) : matrix(single) {};
     Connectivity_matrix(int twin1, int twin2) : matrix (twin1, twin2) {} ;
     Connectivity_matrix(vector<double> thisVectorArray) : matrix (thisVectorArray) {};
-
+ 
 };
